c64c51356c3dd0323d64e8c2fb212b4a970432d5
Govind <govind@mangoitsolutions.com>
2023-08-17 15:07:52 +0530

    Upload complete code

---
+++ .vite/deps/@mui_icons-material_SquareSharp.js
@@ -0,0 +1,31 @@
+"use client";
+import {
+  require_createSvgIcon,
+  require_interopRequireDefault
+} from "./chunk-XCH4UNFR.js";
+import {
+  require_jsx_runtime
+} from "./chunk-OSL3A7XV.js";
+import "./chunk-2PA4WPI3.js";
+import {
+  __commonJS
+} from "./chunk-ROME4SDB.js";
+
+// node_modules/@mui/icons-material/SquareSharp.js
+var require_SquareSharp = __commonJS({
+  "node_modules/@mui/icons-material/SquareSharp.js"(exports) {
+    var _interopRequireDefault = require_interopRequireDefault();
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.default = void 0;
+    var _createSvgIcon = _interopRequireDefault(require_createSvgIcon());
+    var _jsxRuntime = require_jsx_runtime();
+    var _default = (0, _createSvgIcon.default)((0, _jsxRuntime.jsx)("path", {
+      d: "M3 3h18v18H3z"
+    }), "SquareSharp");
+    exports.default = _default;
+  }
+});
+export default require_SquareSharp();
+//# sourceMappingURL=@mui_icons-material_SquareSharp.js.map
